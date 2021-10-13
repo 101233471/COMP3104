a5cb4243d27abc0c61a66f051e0746239e4c94e4
Nicole <nicoletosello@georgebrown.com>
2021-10-13 14:53:57 -0400

    changes

---
+++ build/precache-manifest.47cb8bb57660067903361599438325bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddb34fe3d9f38aad73b4",
+    "url": "/101233471/COMP3104/static/css/main.92d7dd48.chunk.css"
+  },
+  {
+    "revision": "ddb34fe3d9f38aad73b4",
+    "url": "/101233471/COMP3104/static/js/main.ddb34fe3.chunk.js"
+  },
+  {
+    "revision": "307cfffe1ed05ea6d042",
+    "url": "/101233471/COMP3104/static/js/runtime~main.307cfffe.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/101233471/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "b9e36ce7c1ea3ce71c32a9f616f40d2e",
+    "url": "/101233471/COMP3104/index.html"
+  }
+];
